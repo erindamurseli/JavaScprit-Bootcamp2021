38d9560e5eee2d14c8adcba3d1e2a3673cc5cdce
Erinda Murseli <erinda.murseli@student.uni-pr.edu>
2021-09-20 14:22:51 +0200

    DOM and Events

---
+++ 07/image.js
@@ -0,0 +1,20 @@
+// export function generaterandomnumber(){
+// return Math.floor(Math.random()*max);}
+
+// export function createElement(type, attribute=[]) {
+// const element=document.createElement(`${type}`);
+// if(attribute.length>0){
+//     attribute.forEach((attribute,index) => {
+//         element.setAttribute(`${attribute.type}`,attribute.value);
+//     });
+// }
+// return element;
+// }
+
+// export function clearContent(element){
+//     element.innerHTML="";
+// }
+
+// export function appendchilToParent(child,parent){
+//     parent.appendChild(child);
+// }
